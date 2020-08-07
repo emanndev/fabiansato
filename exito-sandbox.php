e4b21cfc2ae9f48df1a5cbb36b96a05debed49f1
fabiansatodev <fabiansato@gmail.com>
2020-08-07 19:41:35 -0300

    Subida todo el proyecto
    Subida todo el proyecto

---
+++ exito-sandbox.php
@@ -0,0 +1,80 @@
+﻿<?php
+
+$error = '';
+
+//VALIDANDO NOMBRE
+$captcha = $_GET["g-recaptcha-response"];
+$captcha = filter_var($captcha, FILTER_SANITIZE_STRING);
+$captcha = trim($captcha);
+
+
+
+$nombre = $_GET["first_name"];
+$nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+$nombre = trim($nombre);
+
+$apellido = $_GET["last_name"];
+$apellido = filter_var($apellido, FILTER_SANITIZE_STRING);
+$apellido = trim($apellido);
+
+
+$email = $_GET["email"];
+$email = filter_var($email,FILTER_SANITIZE_EMAIL);
+
+
+//CUERPO DEL MENSAJE
+$cuerpo .= "Nombre: ";
+$cuerpo .= $nombre;
+$cuerpo .= "\n";
+ 
+$cuerpo .= "Email: ";
+$cuerpo .= $email;
+$cuerpo .= "\n";
+ 
+$cuerpo .= "Mensaje: ";
+$cuerpo .= $apellido;
+$cuerpo .= "\n";
+
+//DIRECCIÓN
+$enviarA = 'fabiansato@gmail.com'; //REEMPLAZAR CON TU CORREO ELECTRÓNICO
+$asunto = 'Nuevo mensaje de mi sitio web';
+
+//ENVIAR CORREO
+if($error == ''){
+    $success = mail($enviarA,$asunto,$cuerpo,'de: '.$email);
+    echo 'exito';
+
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://sandbox.bunkerdb.com/api/v4/consumers",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => false,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS =>" {\r\n    \"first_name\": \"$nombre\",\r\n    \"last_name\": \"$apellido\",\r\n    \"email\": \"$email\"\r\n  } ",
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type: application/json",
+        "Authorization: Bearer 8cf90219c88ce47b14c992ae0512bd0f2c520427"
+        
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+  //  echo $response;
+    echo "formulario enviado...";
+	header("Location: https://ar-promo.unileverservices.com/exito-sandbox");
+    
+    
+
+}else{
+    echo $error;
+}
+
+?>
---
+++ exitoforms/exito-sedal-cosquin.php
@@ -0,0 +1,138 @@
+﻿<?php
+
+$error = '';
+
+//guardando los GETS que recibe el PHP mediante query URL en variables
+$captcha = $_GET["g-recaptcha-response"];
+$captcha = filter_var($captcha, FILTER_SANITIZE_STRING);
+$captcha = trim($captcha);
+
+$first_name= $_GET["first_name"];
+$first_name = filter_var($first_name, FILTER_SANITIZE_STRING);
+$first_name = trim($first_name);
+
+$last_name = $_GET["last_name"];
+$last_name = filter_var($last_name, FILTER_SANITIZE_STRING);
+$last_name = trim($last_name);
+
+$email = $_GET["email"];
+$email = filter_var($email,FILTER_SANITIZE_EMAIL);
+
+$cellphone = $_GET["cellphone"];
+$cellphone = filter_var($cellphone, FILTER_SANITIZE_STRING);
+$cellphone = trim($cellphone);
+
+$document = $_GET["document"];
+$document = filter_var($document, FILTER_SANITIZE_STRING);
+$document = trim($document);
+
+
+
+//validar mayor18
+$mayor_18_anos = is_array($_GET['mayor_18_anos']);
+if ($mayor_18_anos == 1){
+  $mayor_18_anos = 1;
+}else {$mayor_18_anos = 0;}
+
+$allowbrand = is_array($_GET['allow_brand']);
+if ($allowbrand == 1){
+  $allowbrand = 1;
+}else {  $allowbrand = 0;}
+
+//validar allow global
+$allow_global = is_array($_GET['allow_global']);
+if ($allow_global == 1){
+  $allow_global = 1;
+}else {  $allow_global = 0;}
+
+$acepto_bases_y_condiciones = is_array($_GET['acepto_bases_y_condiciones']);
+if ($acepto_bases_y_condiciones == 1){
+  $acepto_bases_y_condiciones = 1;
+}else {  $acepto_bases_y_condiciones = 0;}
+
+//print json en pantalla
+echo "first_name:" .$first_name. "<br>";
+echo "last_name:" .$last_name. "<br>";
+echo "email:" .$email. "<br>";
+
+echo "celular:" .$cellphone. "<br>";
+echo "document:" .$document. "<br>";
+echo "<br>";
+echo "es mayor de 18:" .$allowbrand. "<br>";
+echo "allow_brand:" .$allowbrand. "<br>";
+echo "allow_global:" .$allow_global. "<br>";
+echo "acepta bases y condiciones:" .$acepto_bases_y_condiciones. "<br>";
+
+//CUERPO DEL Mail que se va a enviar
+$cuerpo .= "Nombre: ";
+$cuerpo .= $first_name;
+$cuerpo .= "\n";
+
+$cuerpo .= "Mensaje: ";
+$cuerpo .= $last_name;
+$cuerpo .= "\n";
+
+$cuerpo .= "Email: ";
+$cuerpo .= $email;
+$cuerpo .= "\n";
+
+
+
+
+//DIRECCIÓN
+$enviarA = 'modalsurveyus@gmail.com'; //REEMPLAZAR CON TU CORREO ELECTRÓNICO
+$asunto = 'Formulario Sandbox enviado';
+
+//ENVIAR CORREO
+if($error == ''){
+    $success = mail($enviarA,$asunto,$cuerpo,'de: '.$email);
+    
+
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://ar.eagle-latam.com/api/v4/consumers",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => false,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "POST",
+      CURLOPT_POSTFIELDS =>" {
+        \r\n    
+        \"first_name\": \"$first_name\",\r\n
+        \"last_name\": \"$last_name\",\r\n   
+        \"email\": \"$email\",\r\n 
+        \"cellphone\": \"$cellphone\",\r\n 
+        \"document\": \"$document\",\r\n 
+        \"mayor_18_anos\": \"$mayor_18_anos\",\r\n 
+        \"allow_brand\": \"$allowbrand\",\r\n 
+        \"allow_global\": \"$allow_global\",\r\n 
+        \"allow_brand\": \"$allowbrand\"\r\n 
+     
+
+      } ",
+      CURLOPT_HTTPHEADER => array(
+        "Content-Type: application/json",
+        "Authorization: Bearer 59a217ec204f33cf534cd2fe170af4499e733ee0" //Api cosquin chequado
+        
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+  //  echo $response;
+    echo "Exito <br>";
+    echo "formulario enviado...";
+	header("Location: https://ar-promo.unileverservices.com/campana-de-sedal-mdq-cosquin-exito/");
+    
+    
+
+}else{
+    echo $error;
+}
+
+?>  
